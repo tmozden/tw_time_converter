b37373838fefb638b49fa546e042bbe47ffccdbd
Travis Mozden <mozden09@gmail.com>
2015-12-15 23:25:32 -0600

    Create offset variable for easy use in other timezones.

--- tw_time_converter.py
+++ tw_time_converter.py
@@ -1,14 +1,17 @@
 import re
 def main():
+    #This is the hours offset from tw 
+    time_offset = 18
+
     cmd = ''
     while cmd != 'exit':
         cmd = raw_input("Enter TW time:").strip(" \n").lower()
         the_match = re.match("(\d\d):(\d\d):(\d\d)",cmd)
         if the_match:
             hours = int( the_match.group(1) )
-            am_pm = True if ( ( hours + 18 ) % 24 ) > 11 else False 
+            am_pm = True if ( ( hours + time_offset ) % 24 ) > 11 else False 
             am_pm = "pm" if am_pm else "am"
-            hours = ( hours + 18 ) % 12
+            hours = ( hours + time_offset ) % 12
             print str(hours) +":"+ the_match.group(2) + ":" + the_match.group(3) + am_pm
         else:
             if cmd == 'exit':
